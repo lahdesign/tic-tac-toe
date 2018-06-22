fa51f69707a3f10b6011317bc6f06f871cc3a4d9
Ashley <ashley.ratigan@gmail.com>
2018-06-22 14:54:53 -0400

    Updated ApiUrls
    Changed apiUrls to most up to date form

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,9 +2,11 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  // production: 'https://aqueous-atoll-85096.herokuapp.com',
   // development: 'http://localhost:4741'
-  development: 'http://tic-tac-toe.wdibos.com'
+  // development: 'http://tic-tac-toe.wdibos.com'
+  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  development: 'https://tic-tac-toe-wdi.herokuapp.com/'
 }
 
 if (window.location.hostname === 'localhost') {
