ac791475cc56991767e82ff4dfa46f27c33013b1
Ashley <ashley.ratigan@gmail.com>
2018-06-22 15:46:27 -0400

    Created logic to close modal
    Could not get one function to close both modals, so made two functions, one to close sign up and the other to close sign in

--- assets/scripts/modals.js
+++ assets/scripts/modals.js
@@ -1,9 +1,19 @@
 // Get the modal
-const modal = document.getElementsByClassName('modal')
+const modal = document.getElementById('signUp')
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target === modal) {
     modal.style.display = 'none'
+  }
+}
+
+// Get the modal
+const signIn = document.getElementById('signIn')
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target === signIn) {
+    signIn.style.display = 'none'
   }
 }
