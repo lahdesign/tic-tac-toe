37c6635c159f841cdb942e81385b7402247d5de0
Ashley <ashley.ratigan@gmail.com>
2018-06-23 18:10:02 -0400

    Changed message id to userMessage
    Added new div with id in html and edited id in ui

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,34 +3,34 @@
 const store = require('../store')
 
 const signUpSuccess = function (data) {
-  $('#message').text('Signed up successfully')
-  $('#message').css('background-color', 'green')
+  $('userMessage').text('Signed up successfully')
+  $('userMessage').css('background-color', 'green')
   console.log('signUpSuccess ran. Data is :', data)
 }
 
 const signInSuccess = function (data) {
-  $('#message').text('Signed in successfully')
-  $('#message').css('background-color', 'green')
+  $('userMessage').text('Signed in successfully')
+  $('userMessage').css('background-color', 'green')
   console.log('signInSuccess ran. Data is :', data)
   store.user = data.user
 }
 
 const changePassword = function (data) {
-  $('#message').text('Password changed successfully')
-  $('#message').css('background-color', 'green')
+  $('userMessage').text('Password changed successfully')
+  $('userMessage').css('background-color', 'green')
   console.log('changePassword ran. Data is :', data)
 }
 
 const signOutSuccess = function () {
-  $('#message').text('Signed out successfully')
-  $('#message').css('background-color', 'green')
+  $('userMessage').text('Signed out successfully')
+  $('userMessage').css('background-color', 'green')
   console.log('signOutSuccess ran, and has nothing to return')
   store.user = null
 }
 
 const failure = function (error) {
-  $('#message').text('Error')
-  $('#message').css('background-color', 'red')
+  $('userMessage').text('Error')
+  $('userMessage').css('background-color', 'red')
   console.log('Failure ran. Error is :', error)
 }
 
