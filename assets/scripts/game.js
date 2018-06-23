aff8fa0b5b5734a5925f06ead20e9699216749e9
Ashley <ashley.ratigan@gmail.com>
2018-06-23 13:38:36 -0400

    Added score logic so three in a row is a win
    Added logic to declare winner for 3 in a row for horizantal columns

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -7,38 +7,21 @@
 
 // let squares = ['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9']
 
-const userOne = 'X'
-const userTwo = 'O'
-let currentUser = userOne
+// 1 2 3
+// 4 5 6
+// 7 8 9
+
 // const square = document.getElementsByTagName('td')
 // console.log(square)
 
-// *******************
-// works but can re-click and no end
-// ***********************
-// $('.col1, .col2, .col3').click(function () {
-//   if (currentUser === userOne) {
-//     $(this).html(userTwo)
-//     currentUser = userTwo
-//   } else if (currentUser === userTwo) {
-//     $(this).html(userOne)
-//     currentUser = userOne
-//   }
-// })
+const userOne = 'X'
+const userTwo = 'O'
+let currentUser = userOne
 
-// $('.col1, .col2, .col3').click(function () {
-//   if (currentUser === userOne && square[0].innerHTML === '') {
-//     $(this).html(userTwo)
-//     currentUser = userTwo
-//   } else if (currentUser === userTwo) {
-//     $(this).html(userOne)
-//     currentUser = userOne
-//   }
-// })
-
-const gameboard = $('.col1, .col2, .col3').click(function (square) {
+const playerMoves = $('.col1, .col2, .col3').click(function (square) {
   if (square.target.innerHTML === '') {
     // console.log(square.target.innerHTML)
+    // console.log(square.target.id)
     // console.log(square.target)
     // console.log(square)
     if (currentUser === userOne) {
@@ -49,58 +32,102 @@ const gameboard = $('.col1, .col2, .col3').click(function (square) {
       currentUser = userOne
     }
   }
+//   checkWinner()
+  sv()
 })
 
-function startGame () {
+// ************************
+// function to determine winner
+// ************************
+// const checkWinner = function () {
+//   if (sv(1) !== '' && sv(1) === sv(2) ) {
+//     console.log('you win')
+//   }
+// }
+
+const sq = function (number) {
+  return document.getElementById('s' + number).innerText
+}
+
+// const winner = function () {
+// //   if (sq(1) !== '' && sq(1) === sq(2) === sq(3)) {
+// //     console.log('you win')
+// //   }
+//   if (areEqual(sq(1), sq(2), sq(3))) {
+//     console.log('winner')
+//   }
+// }
+const sv = function () {
+  for (let i = 1; i <= 9; i++) {
+    sq(i)
+  } if (sq(1) !== '' && sq(1) === sq(2) && sq(2) === sq(3)) {
+    console.log(`${sq(1)} wins`)
+  } else if (sq(4) !== '' && sq(4) === sq(5) && sq(5) === sq(6)) {
+    console.log(`${sq(4)} wins`)
+  } else if (sq(7) !== '' && sq(7) === sq(8) && sq(8) === sq(9)) {
+    console.log(`${sq(7)} wins`)
+  } else {
+    console.log('no winner')
+  }
+}
+//   console.log(squares(1))
+//   console.log(squares(2))
+//   if (squares(1) === squares(2) === squares(3)) {
+//     console.log('you win')
+//   }
+// }
+
+
+// const checkWinner = function (move) {
+//   let result = false
+//   if (checkRow(1, 2, 3, move) ||
+//       checkRow(4, 5, 6, move) ||
+//       checkRow(7, 8, 9, move) ||
+//       checkRow(1, 4, 7, move) ||
+//       checkRow(2, 5, 8, move) ||
+//       checkRow(3, 6, 9, move) ||
+//       checkRow(1, 5, 9, move) ||
+//       checkRow(3, 5, 7, move)) {
+
+//     result = true;
+//   }
+//   return result;
+// }
+
+// function checkRow(a, b, c, move) {
+//   var result = false
+//   if (getBox(a) === move && getBox(b) === move && getBox(c) === move) {
+//     result = true
+//   }
+//  return result
+// }
+
+// ************************
+// function to add win to score
+// ************************
+
+// ************************
+// function to start game
+// ************************
+const startGame = function () {
   for (let i = 1; i <= 9; i = i + 1) {
     clearBoard(i)
   }
 }
 
-function clearBoard (number) {
+// ************************
+// function to reset game
+// ************************
+const clearBoard = function (number) {
   document.getElementById('s' + number).innerText = ''
 }
-
 document.getElementById('reset').addEventListener('click', startGame)
 
-// $('.col1, .col2, .col3').click(function (square) {
-//   if (square.target.innerHTML === '') {
-//     if (currentUser === userOne) {
-//       $(this).html(userTwo)
-//       currentUser = userTwo
-//     } else if (currentUser === userTwo) {
-//       $(this).html(userOne)
-//       currentUser = userOne
-//     }
-//   }
-// })
-
-// 0 1 2
-// 3 4 5
-// 6 7 8
-
-// const winner = function () {
-//   if (square[0].innerHTML === square[1].innerHTML === square[2].innerHTML) {
-//     console.log('you win')
-//   } else if (square[3].innerHTML === square[4].innerHTML === square[5].innerHTML) {
-//     console.log('you win')
-//   } else if (square[6].innerHTML === square[7].innerHTML === square[8].innerHTML) {
-//     console.log('you win')
-//   } else if (square[0].innerHTML === square[3].innerHTML === square[6].innerHTML) {
-//     console.log('you win')
-//   } else if (square[1].innerHTML === square[4].innerHTML === square[7].innerHTML) {
-//     console.log('you win')
-//   } else if (square[2].innerHTML === square[5].innerHTML === square[8].innerHTML) {
-//     console.log('you win')
-//   } else if (square[0].innerHTML === square[4].innerHTML === square[8].innerHTML) {
-//     console.log('you win')
-//   } else if (square[6].innerHTML === square[4].innerHTML === square[2].innerHTML) {
-//     console.log('you win')
-//   }
-// }
-
+// ************************
+// exports
+// ************************
 module.exports = {
-  gameboard,
+  playerMoves,
   startGame,
   clearBoard
 }
