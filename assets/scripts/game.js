4e41c07d1e42e6454cdf07adfccca3bf481603e2
Ashley <ashley.ratigan@gmail.com>
2018-06-23 13:59:27 -0400

    Added logic to determine winner in every instance
    Added logic to determine winner for vertical rows and diagonals. Also added message to be displayed for who wins.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -16,92 +16,70 @@
 
 const userOne = 'X'
 const userTwo = 'O'
-let currentUser = userOne
+let currentUser = userTwo
 
 const playerMoves = $('.col1, .col2, .col3').click(function (square) {
-  if (square.target.innerHTML === '') {
-    // console.log(square.target.innerHTML)
-    // console.log(square.target.id)
-    // console.log(square.target)
-    // console.log(square)
-    if (currentUser === userOne) {
-      $(this).html(userTwo)
-      currentUser = userTwo
-    } else if (currentUser === userTwo) {
-      $(this).html(userOne)
-      currentUser = userOne
+  if (checkWinner !== 'no winner') {
+    if (square.target.innerHTML === '') {
+      // console.log(square.target.innerHTML)
+      // console.log(square.target.id)
+      // console.log(square.target)
+      // console.log(square)
+      if (currentUser === userOne) {
+        $(this).html(userTwo)
+        currentUser = userTwo
+      } else if (currentUser === userTwo) {
+        $(this).html(userOne)
+        currentUser = userOne
+      }
     }
   }
-//   checkWinner()
-  sv()
+  checkWinner()
 })
 
 // ************************
 // function to determine winner
 // ************************
-// const checkWinner = function () {
-//   if (sv(1) !== '' && sv(1) === sv(2) ) {
-//     console.log('you win')
-//   }
-// }
-
 const sq = function (number) {
   return document.getElementById('s' + number).innerText
 }
 
-// const winner = function () {
-// //   if (sq(1) !== '' && sq(1) === sq(2) === sq(3)) {
-// //     console.log('you win')
-// //   }
-//   if (areEqual(sq(1), sq(2), sq(3))) {
-//     console.log('winner')
-//   }
-// }
-const sv = function () {
+const checkWinner = function () {
   for (let i = 1; i <= 9; i++) {
     sq(i)
+    // for horizantal rows
   } if (sq(1) !== '' && sq(1) === sq(2) && sq(2) === sq(3)) {
-    console.log(`${sq(1)} wins`)
+    message(`Congratulations, ${sq(1)}! You win!`)
   } else if (sq(4) !== '' && sq(4) === sq(5) && sq(5) === sq(6)) {
-    console.log(`${sq(4)} wins`)
+    message(`Congratulations, ${sq(4)}! You win!`)
   } else if (sq(7) !== '' && sq(7) === sq(8) && sq(8) === sq(9)) {
-    console.log(`${sq(7)} wins`)
+    message(`Congratulations, ${sq(1)}! You win!`)
+    // for vertical rows
+  } else if (sq(1) !== '' && sq(1) === sq(4) && sq(4) === sq(7)) {
+    message(`Congratulations, ${sq(1)}! You win!`)
+  } else if (sq(2) !== '' && sq(2) === sq(5) && sq(5) === sq(8)) {
+    message(`Congratulations, ${sq(2)}! You win!`)
+  } else if (sq(3) !== '' && sq(3) === sq(6) && sq(6) === sq(9)) {
+    message(`Congratulations, ${sq(3)}! You win!`)
+    // for diagonals
+  } else if (sq(1) !== '' && sq(1) === sq(5) && sq(5) === sq(9)) {
+    message(`Congratulations, ${sq(1)}! You win!`)
+  } else if (sq(7) !== '' && sq(7) === sq(5) && sq(5) === sq(3)) {
+    message(`Congratulations, ${sq(7)}! You win!`)
   } else {
-    console.log('no winner')
+    message(`No winner`)
   }
 }
-//   console.log(squares(1))
-//   console.log(squares(2))
-//   if (squares(1) === squares(2) === squares(3)) {
-//     console.log('you win')
+
+const message = function (msg) {
+  document.getElementById('message').innerText = msg
+}
+
+// const end = function () {
+//   if (checkWinner !== 'no winner') {
+//     console.log('no go')
 //   }
 // }
-
-
-// const checkWinner = function (move) {
-//   let result = false
-//   if (checkRow(1, 2, 3, move) ||
-//       checkRow(4, 5, 6, move) ||
-//       checkRow(7, 8, 9, move) ||
-//       checkRow(1, 4, 7, move) ||
-//       checkRow(2, 5, 8, move) ||
-//       checkRow(3, 6, 9, move) ||
-//       checkRow(1, 5, 9, move) ||
-//       checkRow(3, 5, 7, move)) {
-
-//     result = true;
-//   }
-//   return result;
-// }
-
-// function checkRow(a, b, c, move) {
-//   var result = false
-//   if (getBox(a) === move && getBox(b) === move && getBox(c) === move) {
-//     result = true
-//   }
-//  return result
-// }
-
 // ************************
 // function to add win to score
 // ************************
