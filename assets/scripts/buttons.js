17ca81fd18a4aa3e9ea91a82d6121d14df267cec
Ashley <ashley.ratigan@gmail.com>
2018-06-25 16:46:09 -0400

    Added new js file for buttons
    Created new file for js relating to buttons and what is displayed when they are clicked. Added logic to toggle user info

---
+++ assets/scripts/buttons.js
@@ -0,0 +1,10 @@
+// $('#seeInfo').click(function () {
+//   $('.userInfo').css('display', 'none')
+//   $('.userInfo').css('display', 'block')
+// })
+
+$(document).ready(function () {
+  $('#seeInfo').click(function () {
+    $('.userInfo').toggle()
+  })
+})
