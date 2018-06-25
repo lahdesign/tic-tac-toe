32e62de251180232aa0924cc294b8384121fb016
Ashley <ashley.ratigan@gmail.com>
2018-06-25 14:23:59 -0400

    Fixed error with ids
    Forgot to include # infront of a few ids

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,15 +3,16 @@
 const store = require('../store')
 
 const signUpSuccess = function (data) {
-  $('userMessage').text('Signed up successfully')
-  $('userMessage').css('background-color', 'green')
+  $('#userMessage').text('Signed up successfully')
+  $('#userMessage').css('background-color', 'green')
   console.log('signUpSuccess ran. Data is :', data)
 }
 
 const signInSuccess = function (data) {
-  $('userMessage').text('Signed in successfully')
-  $('userMessage').css('background-color', 'green')
+  $('#userMessage').text('Signed in successfully')
+  $('#userMessage').css('background-color', 'green')
   $('#sign-out').removeClass('hidden')
+  $('#change-password').removeClass('hidden')
   $('#play').removeClass('hidden')
   $('#resetScore').removeClass('hidden')
   $('#moves').removeClass('hidden')
@@ -21,14 +22,14 @@ const signInSuccess = function (data) {
 }
 
 const changePassword = function (data) {
-  $('userMessage').text('Password changed successfully')
-  $('userMessage').css('background-color', 'green')
+  $('#userMessage').text('Password changed successfully')
+  $('#userMessage').css('background-color', 'green')
   console.log('changePassword ran. Data is :', data)
 }
 
 const signOutSuccess = function () {
-  $('userMessage').text('Signed out successfully')
-  $('userMessage').css('background-color', 'green')
+  $('#userMessage').text('Signed out successfully')
+  $('#userMessage').css('background-color', 'green')
   console.log('signOutSuccess ran, and has nothing to return')
   store.user = null
 }
@@ -52,8 +53,8 @@ const getGamesSuccess = function (data) {
 }
 
 const failure = function (error) {
-  $('userMessage').text('Error')
-  $('userMessage').css('background-color', 'red')
+  $('#userMessage').text('Error')
+  $('#userMessage').css('background-color', 'red')
   console.log('Failure ran. Error is :', error)
 }
 
