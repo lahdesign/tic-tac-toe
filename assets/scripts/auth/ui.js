c18790c81dc4a58c1a5da765749e67a1e64ae612
Ashley <ashley.ratigan@gmail.com>
2018-06-25 16:24:04 -0400

    Adjusted what is displayed
    Adjusted colors displayed when log in is successful. Also adjusted what is and is not visible when signed in and signed out

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -4,32 +4,38 @@ const store = require('../store')
 
 const signUpSuccess = function (data) {
   $('#userMessage').text('Signed up successfully')
-  $('#userMessage').css('background-color', 'green')
+  $('#userMessage').css('background-color', '888')
   console.log('signUpSuccess ran. Data is :', data)
 }
 
 const signInSuccess = function (data) {
   $('#userMessage').text('Signed in successfully')
-  $('#userMessage').css('background-color', 'green')
+  $('#userMessage').css('background-color', '888')
   $('#sign-out').removeClass('hidden')
-  $('#change-password').removeClass('hidden')
+  $('.userInfo').removeClass('hidden')
+  $('.signInUp').css("display", "none")
+  $('#newGame').removeClass('hidden')
   $('#play').removeClass('hidden')
   $('#resetScore').removeClass('hidden')
-  $('#moves').removeClass('hidden')
-  $('#getGames').removeClass('hidden')
+  // $('#moves').removeClass('hidden')
+  // $('#getGames').removeClass('hidden')
   console.log('signInSuccess ran. Data is :', data)
   store.user = data.user
 }
 
 const changePassword = function (data) {
   $('#userMessage').text('Password changed successfully')
-  $('#userMessage').css('background-color', 'green')
+  $('#userMessage').css('background-color', '#888')
   console.log('changePassword ran. Data is :', data)
 }
 
 const signOutSuccess = function () {
   $('#userMessage').text('Signed out successfully')
-  $('#userMessage').css('background-color', 'green')
+  $('#userMessage').css('background-color', '888')
+  $('.signInUp').css("display", "block")
+  $('#gameboard').css("display", "none")
+  $('.userButtons').css("display", "none")
+  $('.userInfo').css("display", "none")
   console.log('signOutSuccess ran, and has nothing to return')
   store.user = null
 }
