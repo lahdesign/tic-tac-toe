4b41a56e7244e8405de4c47114692ba715ed4e39
Ashley <ashley.ratigan@gmail.com>
2018-06-26 14:40:03 -0400

    Added email validation
    Added email validation regex and buttont to show old games

--- assets/scripts/buttons.js
+++ assets/scripts/buttons.js
@@ -3,14 +3,30 @@
 //   $('.userInfo').css('display', 'block')
 // })
 
+// function to toggle user info
 $(document).ready(function () {
   $('#seeInfo').click(function () {
     $('.userInfo').toggle()
   })
 })
 
+// function to toggle change password form
 $(document).ready(function () {
   $('#changePassword').click(function () {
     $('#change-password').toggle()
   })
 })
+
+// Function to toggle past games
+$(document).ready(function () {
+  $('#getGames').click(function () {
+    $('#view-games').css('display', 'block')
+    // $('#view-games').toggle()
+  })
+})
+
+// Function to validate emails
+const validateEmail = function (email) {
+  const re = /\S+@\S+/
+  return re.test(email)
+}
