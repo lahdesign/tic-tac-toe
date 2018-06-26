367e65e16571a2d22dcfa0f18f5394e5336bc0b3
Ashley <ashley.ratigan@gmail.com>
2018-06-26 12:52:31 -0400

    Added function to check for emails
    Added regex to make sure this is an @ in emails used to sign up

--- assets/scripts/buttons.js
+++ assets/scripts/buttons.js
@@ -3,14 +3,23 @@
 //   $('.userInfo').css('display', 'block')
 // })
 
+// Function to toggle user info
 $(document).ready(function () {
   $('#seeInfo').click(function () {
     $('.userInfo').toggle()
   })
 })
-
+// Function to toggle change password
 $(document).ready(function () {
   $('#changePassword').click(function () {
     $('#change-password').toggle()
   })
 })
+
+// Function to toggle past games
+
+// Function to validate emails
+const validateEmail = function (email) {
+  const re = /\S+@\S+/
+  return re.test(email)
+}
