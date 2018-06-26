7a21b0b70f9171af38a0b7d1095839c44d1f3c04
Ashley <ashley.ratigan@gmail.com>
2018-06-26 14:06:40 -0400

    Changed gameboard to display none
    Changed gameboard to display block when new game is clicked. Removed previosu setting of removing class of hidden

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -11,20 +11,20 @@ const signUpSuccess = function (data) {
 const signInSuccess = function (data) {
   $('#userMessage').text('Signed in successfully')
   $('#userMessage').css('background-color', '888')
-  $('#sign-out').removeClass('hidden')
+  // $('#sign-out').removeClass('hidden')
   // $('.userInfo').removeClass('hidden')
   $('.userButtons').css('display', 'block')
   $('.signInUp').css('display', 'none')
   // $('#moves').removeClass('hidden')
   // $('#getGames').removeClass('hidden')
-  console.log('signInSuccess ran. Data is :', data)
+  // console.log('signInSuccess ran. Data is :', data)
   store.user = data.user
 }
 
 const changePassword = function (data) {
   $('#userMessage').text('Password changed successfully')
   $('#userMessage').css('background-color', '#888')
-  console.log('changePassword ran. Data is :', data)
+  // console.log('changePassword ran. Data is :', data)
 }
 
 const signOutSuccess = function () {
@@ -34,17 +34,17 @@ const signOutSuccess = function () {
   $('#gameboard').css('display', 'none')
   $('.userButtons').css('display', 'none')
   $('.userInfo').css('display', 'none')
-  console.log('signOutSuccess ran, and has nothing to return')
+  // console.log('signOutSuccess ran, and has nothing to return')
   store.user = null
 }
 
 const createSuccess = function (data) {
-  $('#gameboard').removeClass('hidden')
-  console.log('new board created')
-  store.game = data.game
-  store.game.id = data.game.id
-  console.log(store.game)
-  console.log(store.game.id)
+  $('#gameboard').css('display', 'block')
+  // console.log('new board created')
+  // store.game = data.game
+  // store.game.id = data.game.id
+  // console.log(store.game)
+  // console.log(store.game.id)
 }
 
 const movesSuccess = function () {
@@ -55,9 +55,9 @@ const getGamesSuccess = function (data) {
   $('#view-games').css('display', 'block')
   for (let i = 0; i < data.games.length; i++) {
     $('#view-games').append('<p><b>ID:</b>' + data.games[i].id + '  <b>Game Squares</b>' + data.games[i].cells + '</p>')
-    console.log(data.games[i])
+    // console.log(data.games[i])
   }
-  console.log('old games')
+  // console.log('old games')
 }
 
 const failure = function (error) {
